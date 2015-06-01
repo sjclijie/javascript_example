0af538af8579c9599597115767ebbe02df233b75
Jaylee <sjclijie@vip.qq.com>
2015-06-01 14:31:04 +0800

    模块化加载

--- 模块化加载/index.js
+++ 模块化加载/index.js
@@ -27,44 +27,16 @@
             var args = [];
 
             for(var i=0; i<module.dependencies.length; i++){
-                if(!module.entity){
-                    module.entity= moduleMap[module.dependencies[i]].factory();
-                } 
-                console.log(args);
-            }
-
-            console.log(args);
-
-            module.entity = module.factory.apply(null,args);
-            
-            return module.entity;
-
-            /*
-            var module = moduleMap[name];
-
-            console.log(module);
-
-            var args = [];
-
-            console.log(module.dependencies.length);
-            
-            for(var i=0; i<module.dependencies.length; i++){
-                //args.push(moduleMap[module.dependencies[i]].factory());
-                //console.log(moduleMap[module.dependencies[i]].factory());
-                if ( moduleMap[module.dependencies[i].entity] ){
-                    args.push(moduleMap[module.dependencies[i].entity]);
+                if( moduleMap[module.dependencies[i]].entity){
+                    args.push(moduleMap[module.dependencies[i]].entity);
                 } else {
-
                     args.push(this.use(module.dependencies[i]));
                 }
             }
 
-            console.log(args);
+            module.entity = module.factory.apply(null,args);
             
-            module.entity = module.factory.apply(this, args);
-
             return module.entity;
-            */
         }
     }
 
@@ -72,29 +44,32 @@
 
 
 thin.define("A", [], function(){
-    console.log(this);
     return "a";
 });
 
-thin.define("B", [], function(){
-    return "b";
+thin.define("B", ["A"], function(A){
+    return A +"=====";
 });
 
 thin.define("C", [], function(){
     return "c";
 });
 
-thin.define("D", ["A","B","C"], function(A, B, C){
-   return A+B+C; 
+thin.define("D", ["B","C"], function(B, C){
+   return B+C; 
 });
 
 thin.define("E",["A"], function(A){
     return A;
 });
 
-var A = thin.use("A");
-console.log(A);
+//var A = thin.use("A");
+//console.log(A);
 
 //var E = thin.use("E");
 //console.log(E);
+
+var D = thin.use("D");
+
+console.log(D);
 

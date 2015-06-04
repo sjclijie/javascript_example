a67c5d557cb622d74d700ec27e479b7c56ea358d
jaylee <sjclijie@vip.qq.com>
2015-06-04 23:19:15 +0800

    update

--- seajs/dist/main.js
+++ seajs/dist/main.js
@@ -6,8 +6,6 @@
  *
  */
 
-
-
 define("./dist/main", ["module1.js","module2.js"],function(require, exports, module){
 
     var a = require("module1.js").b;  
@@ -18,6 +16,16 @@ define("./dist/main", ["module1.js","module2.js"],function(require, exports, mod
         alert(a);
         alert(b);
     }
+    require.async("module2.js", function(){
+        alert("模块加载完毕");
+    })
+
+    //module是当前模块的相应信息
+
+    console.log(module.id);//模块标识
+    console.log(module.uri);//模块的绝对路径
+    console.log(module.dependencies);//依赖数组
+    console.log(module.exports === exports); //true
 
     exports.show = show;
 });

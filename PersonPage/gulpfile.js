9248b5c099d5766e0280c138c8efb76bdcc0802f
Jaylee <sjclijie@vip.qq.com>
2015-08-28 18:02:23 +0800

    update

--- PersonPage/gulpfile.js
+++ PersonPage/gulpfile.js
@@ -1,4 +1,26 @@
-/**
- * Created by Jaylee on 15/8/28.
- */
+
+var gulp = require("gulp"),
+    connect = require("gulp-connect"),
+    port = process.env.port || 8000;
+
+gulp.task( "html" , function(){
+    gulp.src( "./**/*.html" )
+        .pipe( connect.reload() );
+} );
+
+gulp.task( "watch", function(){
+
+    gulp.watch("./**/*.html", ["html"] );
+
+} );
+
+gulp.task( "connect", function(){
+    connect.server({
+        port: port,
+        livereload: true
+    });
+});
+
+gulp.task( "default", ["connect", "watch"] );
+
 

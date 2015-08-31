15ae0a5ea4c908aff1f0f726fd67b52e4a20803b
Jaylee <sjclijie@vip.qq.com>
2015-08-31 11:28:10 +0800

    update

--- gulpTest/gulpfile.js
+++ gulpTest/gulpfile.js
@@ -2,15 +2,13 @@
  * Created by Jaylee on 15/8/30.
  */
 
-
 var gulp = require("gulp");
 var higo = require("gulp-higo-cdn");
 
 gulp.task("default", function(){
-
     gulp.src("./public/*")
         .pipe(higo({
-            timeout: 1000,
+            timeout: 10000,
         })
     );
 });

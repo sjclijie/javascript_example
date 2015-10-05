5a3fcba280c0f59eb2d85bce8448be72aca7ce04
jaylee <sjclijie@vip.qq.com>
2015-10-05 13:02:52 +0800

    update

--- reactjs/commentBox/app/main.js
+++ reactjs/commentBox/app/main.js
@@ -1,3 +1,34 @@
 /**
  * Created by Jaylee on 15/9/20.
  */
+var React = require("react");
+
+/**
+ * jsx语法
+ */
+var CommentBox = React.createClass({
+    render: function(){
+        return (
+            <div className="commentBox">
+                Hello, world! I am a CommentBox.
+            </div>
+        );
+    }
+});
+
+/*
+非jsx语法
+var CommentBox2 = React.createClass({
+    displayName: "CommentBox",
+    render: function(){
+        return (
+            React.createElement("div", {className:"commentbox"}, "hello world")
+        );
+    }
+});
+*/
+
+React.render(<CommentBox/>, document.querySelector("#app"));
+
+
+
--- reactjs/commentBox/gulpfile.js
+++ reactjs/commentBox/gulpfile.js
@@ -3,18 +3,32 @@
  */
 
 var gulp = require("gulp"),
-    connect = require("gulp-connect"),
+    livereload = require("gulp-livereload"),
+    notify = require("gulp-notify"),
     browserify = require("gulp-browserify");
 
 
 gulp.task("browserify", function(){
-
-    return gulp.src("app/**/main.js")
-                .pipe(browserify({
-                    transform: "reactify"
-                })).
-                pipe(gulp.dest("./dist/"));
+    return gulp.src(["app/**/main.js", "!node_modules/**/*"])
+        .pipe(browserify({
+                transform: "reactify"
+            }))
+        .pipe(gulp.dest("./dist/"))
+        .pipe(livereload())
+        .pipe(notify({"message":"reactify complete."}))
 });
 
+gulp.task("otherFiles", function(){
+    return gulp.src(["**","!**/*.js", "!node_modules/**/*"])
+        .pipe(livereload());
+});
+
+gulp.task("watch", function(){
+
+    livereload.listen();
+
+    gulp.watch(["app/**/*.js","!node_modules/**/*"], ["browserify"]);
+    gulp.watch(["**","!**/*.js", "!node_modules/**/*"], ["otherFiles"]);
+});
 
 

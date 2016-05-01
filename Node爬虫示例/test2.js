b0c755eb9c278bbe4b553bbc0a0e769fb2c14785
jaylee <sjclijie@vip.qq.com>
2016-05-01 20:14:26 +0800

    update

--- Node爬虫示例/test2.js
+++ Node爬虫示例/test2.js
@@ -86,51 +86,7 @@ async function handleDir(dir){
     } catch(err) {
         console.log("error occurs when readDir", dir, err);
     }
-
-
-
-    /*
-    fs.readdir(dir, function(err, files){
-
-        files.map( file => {
-
-            let fullFileName = path.join(dir, file);
-
-            fs.stat(fullFileName, function(err, stats){
-
-                if (err ){
-                    console.error("error occurs when processing", file, err);
-                    return;
-                }
-
-                if (stats.isFile() && path.extname(file) == extName){
-
-                    let thisCount = counter++;
-
-                    //打开始log
-                    console.log("start processing", fullFileName, '[' + thisCount + ']' );
-
-                    fs.readFile( fullFileName, "utf-8", function(err, fileString){
-                        if (err){
-                            console.error("error occurs when processing", file, err);
-                        }
-                        //打结束log
-                       console.log("finish processing", fullFileName, '['+ thisCount+']'); 
-                    });
-                } else if ( stats.isDirectory() ){
-                    handleDir( fullFileName );
-                }
-            });
-        } );
-    });
-    */
 }
 
 
 handleDir("/Users/Jaylee/ng_sta/src");
-
-
-
-
-
-

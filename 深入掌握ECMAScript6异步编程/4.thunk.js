a0eb746251d1060a25230cbd7f16cf716ca67d13
jaylee <sjclijie@vip.qq.com>
2016-05-02 15:07:39 +0800

    update

--- 深入掌握ECMAScript6异步编程/4.thunk.js
+++ 深入掌握ECMAScript6异步编程/4.thunk.js
@@ -1,5 +1,20 @@
-
-
 # 编译器"传名调用"的实现，是将参数放到一个临时的函数中，再将这个临时函数传入到函数体中，这个临时函数就叫thunk函数
+
+
+function f(m){
+    return m*2;
+} 
+
+f( x + 5);
+
+//等同于
+
+var thunk = function(){
+    return x + 5;
+}
+
+function f(m){
+    return m()*2;
+}
 
 

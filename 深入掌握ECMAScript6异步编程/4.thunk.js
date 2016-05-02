eb282c4ee5b13a2dc11d710cead2679e3eb49803
jaylee <sjclijie@vip.qq.com>
2016-05-02 17:32:59 +0800

    update

--- 深入掌握ECMAScript6异步编程/4.thunk.js
+++ 深入掌握ECMAScript6异步编程/4.thunk.js
@@ -1,6 +1,5 @@
 # 编译器"传名调用"的实现，是将参数放到一个临时的函数中，再将这个临时函数传入到函数体中，这个临时函数就叫thunk函数
 
-
 function f(m){
     return m*2;
 } 
@@ -16,5 +15,40 @@ var thunk = function(){
 function f(m){
     return m()*2;
 }
+
+
+# 在javascript中，Thunk函数替换的不是表达式，而是多参数函数，将其替换成单参数的版本，且只接受回调函数作为参数。
+
+//正常多从参数版本
+fs.readFile( filename, callback);
+
+//Thunk版本的readFile
+var readFileThunk = Thunk( fileName );
+readFileThunk(callback);
+
+var Thunk = function( fileName ){
+    return function( callback ){
+        return fs.readFile( fileName, callback);
+    }
+}
+
+//thunk函数包装器
+
+var Thunk = function(fn){
+
+    return function(){
+        var args = Array.prototype.slice.call(arguments);
+        return function( callback ){
+            args.push( callback );
+            return fn.apply(this, args);
+        }
+    }
+}
+
+var readFileThunk = Thunk( fs.readFile );
+
+readFile( fileName )( callback );
+
+
 
 
